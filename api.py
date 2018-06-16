bfeef451f2946311b3784e785a9576d584117fab
ibrahy <ibra.barry77@gmail.com>
2018-06-16 03:16:49 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,32 +1,34 @@
 # filename: api.py
 """A basic (single function) API written using hug"""
 import hug
-import mysql.connector
-import json
+import mysql.connector ,json
 
 
 
 
-@hug.get('/allPokemon')
-def allPokemon(id):
-    conn = mysql.connector.connect(host="localhost", user="root", password="", database="pokemon")
+
+@hug.get('/' output=hug.output_format.json))
+def getPokemon(id):
+    conn = mysql.connector.connect(host="localhost", user="root", password="", database="pokemones")
     cursor = conn.cursor()
     cursor.execute("""select nom, p_type, total from pokemones where id = %s""", [id])
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print('{0} | {1} | {2}'.format(row[0], row[1], row[2]))
+    res = cursor.fetchall()
+    
+    return json.dumps(res)
+    
     conn.close()
     return rows
 
-@hug.post('/Pokemon')
+@hug.delete('/' output=hug.output_format.json))
 def Pokemon():
-    conn = mysql.connector.connect(host="localhost", user="root", password="", database="pokemon")
-    cursor = conn.cursor()
-    cursor.execute("""INSERT INTO pokemones (id, nom, p_type, total, hp, attack, defense, speed_att, speed_def, speed) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""", user)
-    
-    conn.commit()
-    conn.close()
+    cursor = cnx.cursor()
+    print(body.get("id"))
+    try:
+        cursor.execute("""DELETE FROM pokemon WHERE id = %s""", [str(body.get("id"))])
+        conn.commit()
+    except:
+        return "ERROR"
 
+    return json.dumps("it work")
 
 
